81a7ee9bf37c679350245ff7d3e8ddf78efca716
niya-s <niya.somkerd@gmail.com>
2025-04-08 18:11:46 +0700

    feat: Implement debounced search functionality in SearchMenu component

--- components/Navbar/SearchMenu.tsx
+++ components/Navbar/SearchMenu.tsx
@@ -1,12 +1,50 @@
-import { Input } from "../ui/input"
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { Input } from "../ui/input";
+import { useEffect, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 const SearchMenu = () => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+
+  const [search, setSearch] = useState(
+    searchParams.get("search")?.toString() || ""
+  );
+
+  const hdlSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+
+    // url change
+    replace(`/?${params.toString()}`);
+  }, 500);
+
+  useEffect(() => {
+    //
+    if (!searchParams.get("search")) {
+      setSearch("");
+    }
+  }, [searchParams.get("search")]);
+
+  // console.log(searchParams.get("search"));
+
   return (
-    <Input 
-    type="text"
-    placeholder="Search"
-    className="max-w-md"
+    <Input
+      type="text"
+      placeholder="Search"
+      className="max-w-md"
+      onChange={(e) => {
+        setSearch(e.target.value);
+        hdlSearch(e.target.value);
+      }}
+      value={search}
     />
-  )
-}
-export default SearchMenu
+  );
+};
+export default SearchMenu;
