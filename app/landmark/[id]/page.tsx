9e34dcb395d34329c1eee0aa2dc871fc1a1a3d12
niya-s <niya.somkerd@gmail.com>
2025-05-13 12:28:30 +0700

    fix: type error: params is not promise

--- app/landmark/[id]/page.tsx
+++ app/landmark/[id]/page.tsx
@@ -7,7 +7,13 @@ import ImageContainer from "@/components/landmark/ImageContainer";
 import ShareBtn from "@/components/landmark/ShareBtn";
 import { redirect } from "next/navigation";
 
-const LandmarkDetail = async ({ params }: { params: { id: string } }) => {
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+const LandmarkDetail = async ({ params }: PageProps) => {
   const { id } = params;
   const landmark = await fetchLandmarkDetail({ id });
 
